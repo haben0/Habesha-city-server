c9c65fba8e232c3ab85bd17c067c7f9f331b3d84
haben0 <80583582+haben0@users.noreply.github.com>
2022-02-03 18:46:18 +0000

    added controller

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,8 +1,27 @@
+import ActivityMessage from '../models/activitiyMessage.js';
 
-export const getActivities = (req, res) => {
-    res.send('Get Activities');
+export const getActivities = async (req, res) => {
+    try {
+        const activitiesMessages = await ActivityMessage.find();
+
+        console.log(activitiesMessages);
+
+        res.status(200).json(activitiesMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
-export const createActivity = (req, res) => {
-    res.send('Activity Creation');
+export const createActivity = async (req, res) => {
+    const activity = req.body;
+    
+    const newActivity = new ActivityMessage(activity);
+
+    try {
+        await newActivity.save();
+
+        res.status(201).json(newActivity);        
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
 }
